8601ad04bdc4517729a6a5d264daae39778984c4
Jurgen hace programas <jurgenfbermudezpicado@gmail.com>
2023-09-11 23:59:45 -0600

    Cambios que el profesor indico, ya realizados

--- Screens/Estructura_Arquitectura.xaml.cs
+++ Screens/Estructura_Arquitectura.xaml.cs
@@ -23,6 +23,25 @@ namespace Simulador.Screens
         public Estructura_Arquitectura()
         {
             InitializeComponent();
+            int cantReg = 8; // Cantidad de registro en el procesador
+            Registro[] registros = new Registro[cantReg];
+
+            for (int i=0; i< cantReg; i++) // Genera los registros necesarios para el procesador
+            {
+                registros[i] = new Registro 
+                {
+                    NumReg = $"R{i}", // Le da su numero de registro
+                    Valor = (i + 1) * 100 // Le da un valor al registro
+                };
+
+                DataGridReg.Items.Add(registros[i]); // Agrega el registro creado al DataGrig
+            }
         }
+
+        public class Registro 
+        {
+            public string NumReg { get; set; }
+            public int Valor { get; set; }
+        } 
     }
 }
